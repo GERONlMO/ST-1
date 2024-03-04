1fea9b03386f576f1906250f615ecf335f339400
Ivan Novostroev <54308529+GERONlMO@users.noreply.github.com>
2024-03-04 16:15:51 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,33 +3,33 @@
 #include <cstdint>
 #include "alg.h"
 
-TEST(PrimeTest, CheckPrimeWithPrimeNumber) {
+TEST(TestTest, CheckPrimeWithTestTestNumber) {
   uint64_t primeNumber = 17;
   EXPECT_TRUE(checkPrime(primeNumber));
 }
 
 
-TEST(PrimeTest, CheckPrimeWithCompositeNumber) {
+TEST(TestTest, CheckTestTestWithCompositeNumber) {
   uint64_t compositeNumber = 15;
   EXPECT_FALSE(checkPrime(compositeNumber));
 }
 
 
-TEST(PrimeTest, NthPrimeNumber) {
+TEST(TestTest, NthTestTestNumber) {
   uint64_t n = 6;
   uint64_t expected = 13;
   EXPECT_EQ(expected, nPrime(n));
 }
 
 
-TEST(PrimeTest, NextPrimeNumber) {
+TEST(TestTest, NextTestTestNumber) {
   uint64_t value = 20;
   uint64_t expected = 23;
   EXPECT_EQ(expected, nextPrime(value));
 }
 
 
-TEST(PrimeTest, SumOfPrimeNumbers) {
+TEST(TestTest, SumOfTestTestNumbers) {
   uint64_t hbound = 20;
   uint64_t expectedSum = 2 + 3 + 5 + 7 + 11 + 13 + 17 + 19;
   EXPECT_EQ(expectedSum, sumPrime(hbound));
